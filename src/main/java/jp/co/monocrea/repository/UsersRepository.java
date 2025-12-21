86efbb23146d2aa03937ec1a5333ea1bd8574e25
N S <hallohallo0916@gmail.com>
2025-12-21 17:19:32 +0900

    first commit

---
+++ src/main/java/jp/co/monocrea/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package jp.co.monocrea.repository;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jp.co.monocrea.entity.Users;
+
+public class UsersRepository implements PanacheRepository<Users> {
+    
+}
